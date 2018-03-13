7705a9480d7858ea5d7550ac7ddede29aa06500c
Your Name <you@example.com>
2018-03-13 14:22:34 +0100

    modification du schema

--- modele/todo_modele.js
+++ modele/todo_modele.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var todoSchema = mongoose.Schema({
        "Nom": String,
        "Description": String,
-       "Date": Date,
+       "Date": String,
        "Etat": Boolean,
 })
 
